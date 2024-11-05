6d270fbe18973da128a6fc8b15327ec308728abd
Omar <omarsuleiman739@gmail.com>
2024-11-05 19:40:58 +0200

    Add database connection and related code

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import env from "dotenv";
 
 const app = express();
 const port = 4000;
 const saltRounds = 10;
+env.config();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// const db = new pg.Client({
-//     user: process.env.PG_USER,
-//     host: process.env.PG_HOST,
-//     database: process.env.PG_DATABASE,
-//     password: process.env.PG_PASSWORD,
-//     port: process.env.PG_PORT,
-//   });
-//   db.connect();
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  db.connect();
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
   });
@@ -29,6 +32,8 @@ app.get("/register",(req,res)=>{
 app.get("/login",(req,res)=>{
     res.render("login.ejs");
 });
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
