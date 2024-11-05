f167f2f8912d5feaa6c4f08adb28191280aa74a6
Omar <omarsuleiman739@gmail.com>
2024-11-05 18:59:49 +0200

    Add route to render index.ejs and install npm ejs

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+// const db = new pg.Client({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT,
+//   });
+//   db.connect();
+app.get("/", (req, res) => {
+    res.render("index.ejs");
   });
-  db.connect();
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
